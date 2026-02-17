e9fa39a15dfd9532b1cd1d2484dd966c28fcc401
riccardo bettili <riccardobettili1@gmail.com>
2026-02-18 00:58:09 +0100

    last version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,45 +1,50 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
+import Head from 'expo-router/head';
 
 export default function Layout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: { backgroundColor: '#000' },
-        tabBarActiveTintColor: '#1E90FF',
-        tabBarInactiveTintColor: '#888',
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
+    <>
+      <Head>
+        <title>PitGo</title>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: { backgroundColor: '#000' },
+          tabBarActiveTintColor: '#1E90FF',
+          tabBarInactiveTintColor: '#888',
         }}
-      />
-
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="car" color={color} size={size} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="modal"
-        options={{
-          title: 'Modal',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="alert-circle" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Explore',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="car" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="modal"
+          options={{
+            title: 'Modal',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="alert-circle" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
